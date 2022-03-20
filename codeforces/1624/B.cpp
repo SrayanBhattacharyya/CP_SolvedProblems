1f51482bbb9f45b5deec8a6b48ecfb83ea3bcd8c
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-03-20 22:51:00 +0530

    Add solution for problem `B - Make AP` Link: https://codeforces.com/contest/1624/problem/B Tags: implementation, math, *900 Ref: https://codeforces.com/contest/1624/submission/150290093

--- codeforces/1624/B.cpp
+++ codeforces/1624/B.cpp
@@ -4,19 +4,18 @@ using namespace std;
 #define rev(i,n) for(int i=n;i>=0;i--)
 #define rep_a(i,a,n) for(int i=a;i<n;i++)
 //Srayan Bhattacharyya JU EE
-
 int main()
 {
     int t; cin >> t;
     while (t--)
     {
         int a, b, c; cin >> a >> b >> c;
-        int m = 0;
-        if ((2*b - c) % a == 0 & 2*b - c > 0) m = 1;
-        else if ((a + c) % (2*b) == 0) m = 1;
-        else if ((2 * b - a) % c == 0 && (2 * b- a) > 0) m = 1;
-        if (m) cout << "YES" << "\n";
-        else cout << "NO" << "\n";
-    }    
+        int t1 = 2*b - c;
+        int t2 = a  + (c -a)/2; int t3 = 2*(b - a) + a;
+        if (t1 >= a && t1 % a == 0 && t1 > 0) cout << "YES" << "\n";
+        else if (t2 >= b && t2 % b == 0 && t2 > 0 && (c-a) % 2 == 0) cout << "YES" << "\n";
+        else if (t3 >= c && t3 % c == 0 && t3 > 0) cout << "YES" << "\n";
+        else cout << "NO" << "\n";  
+    }
     return 0;
 }
