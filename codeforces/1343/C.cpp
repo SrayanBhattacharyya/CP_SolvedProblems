60fe4891d9c05d79171c6941315880b809a9c219
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-04-08 19:30:00 +0530

    Add solution for problem `C - Alternating Subsequence` Link: https://codeforces.com/contest/1343/problem/C Tags: dp, greedy, two pointers, *1200 Ref: https://codeforces.com/contest/1343/submission/153017363

--- codeforces/1343/C.cpp
+++ codeforces/1343/C.cpp
@@ -6,51 +6,65 @@ using namespace std;
 typedef long long ll;
 //Srayan Bhattacharyya JU EE
 int main()
-
 {
-    int t; cin >> t;
-    while (t--){
-        int n; cin >> n; int a[n];
-        rep(i, n) cin >> a[i];
-        if (n == 1){
-            cout << a[0] << "\n";
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>a(n);
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        if(n==1){
+            cout<<a[0]<<"\n";
             continue;
-        }
-        int flag = 0; int ans = a[0];
-        rep(i, n){
-            if (a[i] > 0){
-                flag = 1; break;
             }
-            ans = max(ans, a[i]);
+        int flag=0;
+            
+        int ans=a[0];
+        for(int i=0;i<n;i++){
+        if(a[i]>0){
+            flag=1;
+            break;
         }
-        if (flag == 0){
-            cout << ans << "\n"; continue;
-        }
-        int sign; ans = a[0];
-        if (a[0] > 0) sign = 1;
-        else sign = -1;
-        ll total_max = 0;
-        rep(i, n){
-            if ((sign == 1 && a[i] > 0) || (sign == -1 && a[i] < 0)){
-                ans = max(ans, a[i]);
+        ans=max(ans,a[i]);
+    }
+            if(flag==0){
+                cout<<ans<<"\n";
+                continue;
+            }
+            int sign;
+            ans=a[0];
+            if(a[0]>0){
+                sign=1;
             }
             else{
-                total_max += ans;
-                ans = a[i];
-                if (sign == 1) sign = -1;
-                else sign = 1;      
+                sign=-1;
             }
-            if (i == n - 1){
-                if ((a[n-1]> 0 && a[n-2] > 0) || (a[n-1] < 0 && a[n-2] < 0)){
-                    ans = max(ans, a[i]);
-                    total_max += ans;
+            long long total_max=0;
+            for(int i=0;i<n;i++){
+                if((sign==1 && a[i]>0) || (sign==-1 && a[i]<0)){
+                    ans=max(ans,a[i]);
                 }
                 else{
-                    total_max += a[n-1];
+                    total_max+=ans;
+                    ans=a[i];
+                    if(sign==1)
+                        sign=-1;
+                    else
+                        sign=1;
                 }
+                if(i==n-1){
+                    if((a[n-1]>0 && a[n-2]>0) || (a[n-1]<0 && a[n-2]<0)){
+                        ans=max(ans,a[i]);
+                        total_max+=ans;
+                }
+                else
+                    total_max+=a[n-1];
             }
+            }
+            cout<<total_max<<"\n";
         }
-        cout << total_max << "\n";
-    }       
+        
     return 0;
 }
