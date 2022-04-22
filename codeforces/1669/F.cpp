44d59a924897d0300bb9e09f6c98a073a4b4b049
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-04-23 00:20:00 +0530

    Add solution for problem `F - Eating Candies` Link: https://codeforces.com/contest/1669/problem/F Tags: binary search, data structures, greedy, two pointers, *1100 Ref: https://codeforces.com/contest/1669/submission/154589452

---
+++ codeforces/1669/F.cpp
@@ -0,0 +1,51 @@
+//Srayan Bhattacharyya JU EE
+#include<bits/stdc++.h>
+using namespace std;
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rev(i,n) for(int i=n;i>=0;i--)
+#define rep_a(i,a,n) for(int i=a;i<n;i++)
+#define mods 998244353
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef vector <int> vi;
+typedef vector <string> vs;
+typedef vector <ll> vll;
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define st(s) sort(s.begin(),s.end())
+#define dst(s) sort(s.begin() , s.end() , greater<int>())
+const ll mod = 1000000007;
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll mod_mul(ll a, ll b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+ll mod_add(ll a, ll b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+ll inv(ll i) {if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+ll pwr(ll a, ll b) { ll res = 1; while (b > 0) {if (b & 1) res = res * a ; a = a * a; b >>= 1;} return res;}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int t; cin >> t;
+    while (t--){
+        int n; cin >> n; vi a(n);
+        rep(i, n) cin >> a[i];
+        int ans = 0, i = 0,  j = n - 1, left = 0, right = 0;
+        while (i <= j){
+            if(left == right && left > 0) ans = n - j + i - 1;
+            if(left == right) {
+                left += a[i]; i++;
+            }
+            else if (left > right){
+                right += a[j]; j--;
+            }
+            else{
+                left += a[i]; i++;
+            }
+        }
+        if (left == right) ans = n;
+        cout << ans << "\n";
+    }
+    return 0;
+}
