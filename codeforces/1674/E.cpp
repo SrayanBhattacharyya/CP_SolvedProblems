d5923ed940588244d774fdf6be273647c4e451b3
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-05-08 00:45:00 +0530

    Add solution for problem `E - Breaking the Wall` Link: https://codeforces.com/contest/1674/problem/E Tags: binary search, brute force, constructive algorithms, greedy, math, *2000 Ref: https://codeforces.com/contest/1674/submission/156230276

---
+++ codeforces/1674/E.cpp
@@ -0,0 +1,104 @@
+//Srayan Bhattacharyya JU EE
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rev(i,n) for(int i=n;i>=0;i--)
+#define rep_a(i,a,n) for(int i=a;i<n;i++)
+#define mods 998244353
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef vector <int> vi;
+typedef vector <string> vs;
+typedef set <int> si;
+typedef vector <pii> vpii;
+typedef vector <ll> vll;
+#define max3(a,b,c) max(max((a),(b)),(c))
+#define min3(a,b,c) min(min((a),(b)),(c))
+#define st(s) sort(s.begin(),s.end())
+#define dst(s) sort(s.begin() , s.end() , greater<int>())
+#define all(s) s.begin(), s.end()
+#define INT_MAX 1e18
+#define yes cout << "YES" << "\n"
+#define no cout << "NO" << "\n"
+#define print(arr) for (auto it = arr.begin(); it != arr.end(); ++it) cout << *it << " "; cout << "\n";
+const int MOD = 1e9 + 7;
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll mod_mul(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a * b) % MOD) + MOD) % MOD;}
+ll mod_add(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a + b) % MOD) + MOD) % MOD;}
+ll inv(ll i) {if (i == 1) return 1; return (MOD - ((MOD / i) * inv(MOD % i)) % MOD) % MOD;}
+ll pwr(ll a, ll b) { ll res = 1; while (b > 0) {if (b & 1) res = res * a ; a = a * a; b >>= 1;} return res;}
+
+vs split(string s, string del = " "){
+    vs ans; ll start = 0; ll end = s.find(del);
+    while (end != -1) {
+        cout << s.substr(start, end - start) << "\n";
+        start = end + del.size(); 
+        end = s.find(del, start);
+    }
+    ans.push_back(s.substr(start, end - start)); 
+    return ans;
+}
+
+vi divisors(int n){
+    vi v;
+    for (int i = 1; i <= sqrt(n); i++){
+        if (n % i == 0){
+            v.emplace_back(i);
+            if ((n / i) != i) v.emplace_back(n / i);
+        }
+    }
+    return v;
+}
+
+vi findPrimeFactors(int n)
+{
+    vi primeFactors;
+    while (n%2== 0) primeFactors.push_back(2), n = n/2;
+    for (int i = 3; i*i <= n; i = i+2)
+    {
+        while (n%i == 0)
+        {
+            primeFactors.push_back(i);
+            n = n/i;
+        }
+    }
+    if (n > 2) primeFactors.push_back(n);
+    return primeFactors;
+}
+
+bool isPrime(int n)
+{
+    if(n==2 or n==3) return true;
+    if(n%2==0) return false;
+    for(ll i=3; i*i<=n; i+=2) if(n%i==0) return false;
+    return true;
+}
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    int n; cin >> n;
+        vi a(n + 1, 0), b(n + 1, 0);
+        rep_a(i, 1, n+1){
+            cin >> a[i];
+            b[i] = a[i];
+        }
+        sort(all(b));
+        int ans = ceil_div(b[1], 2) + ceil_div(b[2], 2);
+        rep_a(i, 2, n){
+            ans = min(ans, max(a[i-1], a[i+1]));
+            if((a[i-1] & 1) or (a[i+1] & 1)) ans = min(ans,(a[i-1]/2 + a[i+1]/2 + 1));
+        }
+        rep_a(i, 1, n){
+            ll mn,mx;
+            mn = min(a[i],a[i+1]);
+            mx = max(a[i],a[i+1]);
+            if(mx > 2*mn) ans = min(ans,(mx+1)/2);
+            else ans = min(ans,(mn+mx+2)/3);
+        }
+        cout << ans << "\n";
+    return 0;
+}
