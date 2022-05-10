9860dc364c45f1d7eb9d9b36c18184e1da4ec88c
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-05-10 20:18:00 +0530

    Add solution for problem `A - Lucky?` Link: https://codeforces.com/contest/1676/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1676/submission/156628098

---
+++ codeforces/1676/A.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long double
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rev(i,n) for(ll i=n;i>=0;i--)
+#define rep_a(i,a,n) for(ll i=a;i<n;i++)
+#define mods 998244353
+typedef pair <ll, ll> pll;
+typedef vector <ll> vll;
+typedef vector <vll> vvlll;
+typedef vector <string> vs;
+typedef set <ll> sll;
+typedef vector <pll> vpll;
+typedef vector <long double> vld;
+#define st(s) sort(s.begin(),s.end())
+#define dst(s) sort(s.begin() , s.end() , greater<int>())
+#define all(s) s.begin(), s.end()
+#define yes cout << "YES" << "\n"
+#define no cout << "NO" << "\n"
+#define print(arr) for (auto it = arr.begin(); it != arr.end(); ++it) cout << *it << " "; cout << "\n";
+const ll MOD = 1e9 + 7; 
+const ll MODS = 998244353; 
+const ll N = 1e7 + 10; 
+const ll LLMAX = 1e18; 
+const lld pi = 3.14159265358979323846;
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll mod_mul(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a * b) % MOD) + MOD) % MOD;}
+ll mod_add(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a + b) % MOD) + MOD) % MOD;}
+ll inv(ll i) {if (i == 1) return 1; return (MOD - ((MOD / i) * inv(MOD % i)) % MOD) % MOD;}
+ll pwr(ll a, ll b) { ll res = 1; while (b > 0) {if (b & 1) res = res * a ; a = a * a; b >>= 1;} return res;}
+
+vs split(string s, string del = " "){
+    vs ans; ll start = 0; ll end = s.find(del);
+    while (end != -1) {
+        cout << s.substr(start, end - start) << "\n";
+        start = end + del.size(); 
+        end = s.find(del, start);
+    }
+    ans.push_back(s.substr(start, end - start)); 
+    return ans;
+}
+
+vll divisors(ll n){
+    vll v;
+    for (ll i = 1; i <= sqrt(n); i++){
+        if (n % i == 0){
+            v.emplace_back(i);
+            if ((n / i) != i) v.emplace_back(n / i);
+        }
+    }
+    return v;
+}
+
+vll findPrimeFactors(ll n)
+{
+    vll primeFactors;
+    while (n%2== 0) primeFactors.push_back(2), n = n/2;
+    for (ll i = 3; i*i <= n; i = i+2)
+    {
+        while (n%i == 0)
+        {
+            primeFactors.push_back(i);
+            n = n/i;
+        }
+    }
+    if (n > 2) primeFactors.push_back(n);
+    return primeFactors;
+}
+
+bool isPrime(ll n)
+{
+    if(n==2 or n==3) return true;
+    if(n%2==0) return false;
+    for(ll i=3; i*i<=n; i+=2) if(n%i==0) return false;
+    return true;
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    while (t--){
+        string s; cin >> s;
+        ll s1 = s[0] + s[1] + s[2] - (3 * '0');
+        ll s2 = s[3] + s[4] + s[5] - (3 * '0');
+        if (s1 == s2) cout << "YES" << "\n";
+        else cout << "NO" << "\n";
+    }
+    return 0;
+}
