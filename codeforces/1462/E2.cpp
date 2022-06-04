240b9724179e7f9f8d7a49eac30b1acc7c0616b7
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-06-04 14:02:00 +0530

    Add solution for problem `E2 - Close Tuples (hard version)` Link: https://codeforces.com/contest/1462/problem/E2 Tags: binary search, combinatorics, implementation, math, sortings, two pointers, *1700 Ref: https://codeforces.com/contest/1462/submission/159457379

---
+++ codeforces/1462/E2.cpp
@@ -0,0 +1,168 @@
+//Srayan Bhattachrayya JU EE
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long double
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rev(i,n) for(ll i=n;i>=0;i--)
+#define rev_a(i,a,n) for(ll i=n;i>=a;i--)
+#define rep_a(i,a,n) for(ll i=a;i<n;i++)
+typedef pair <ll, ll> pll;
+typedef vector <ll> vll;
+typedef vector <vll> vvll;
+typedef vector <string> vs;
+typedef set <ll> sll;
+typedef vector <pll> vpll;
+typedef vector <long double> vld;
+#define st(s) sort(s.begin(),s.end())
+#define dst(s) sort(s.begin() , s.end() , greater<int>())
+#define all(s) s.begin(), s.end()
+#define yes cout << "YES" << "\n"
+#define no cout << "NO" << "\n"
+#define print(arr) for (auto it = arr.begin(); it != arr.end(); ++it) cout << *it << " "; cout << "\n";
+const ll MOD = 1e9 + 7; 
+const ll MODS = 998244353; 
+const ll NN = 1e7 + 10; 
+const ll NS = 1e5 + 10;
+const ll LLMAX = 1e18; 
+const lld pi = 3.14159265358979323846;
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll mod_mul(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a * b) % MOD) + MOD) % MOD;}
+ll mods_mul(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a * b) % MODS) + MODS) % MODS;}
+ll mod_add(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a + b) % MOD) + MOD) % MOD;}
+ll mod_inv(ll i) {if (i == 1) return 1; return (MOD - ((MOD / i) * mod_inv(MOD % i)) % MOD) % MOD;}
+ll mod_pwr(ll a, ll b) { if(b < 0) return 0; ll res = 1; while (b > 0) {if (b & 1) res = res * a % MOD; a = a * a % MOD; b >>= 1;} return res;}
+
+vs split(string s, string del = " "){
+    vs ans; ll start = 0; ll end = s.find(del);
+    while (end != -1) {
+        cout << s.substr(start, end - start) << "\n";
+        start = end + del.size(); 
+        end = s.find(del, start);
+    }
+    ans.push_back(s.substr(start, end - start)); 
+    return ans;
+}
+
+vll divisors(ll n){
+    vll v;
+    for (ll i = 1; i <= (ll)sqrt(n); i++){
+        if (n % i == 0){
+            v.emplace_back(i);
+            if ((n / i) != i) v.emplace_back(n / i);
+        }
+    }
+    return v;
+}
+
+vll findPrimeFactors(ll n){
+    vll primeFactors;
+    while (n%2== 0) primeFactors.push_back(2), n = n/2;
+    for (ll i = 3; i*i <= n; i = i+2)
+    {
+        while (n%i == 0)
+        {   
+            primeFactors.push_back(i);
+            n = n/i;
+        }
+    }
+    if (n > 2) primeFactors.push_back(n);
+    return primeFactors;
+}
+
+bool isPrime(ll n){
+	if (n == 1) return false;
+    if(n==2 or n==3) return true;
+    if(n%2==0) return false;
+    for(ll i=3; i * i <= n; i += 2) if(n % i==0) return false;
+    return true;
+}
+
+ll *fact;
+void calFact(){
+    fact = new ll[NN];
+    fact[0] = 1;
+    fact[1] = 1;
+    for (ll i = 2; i < NN; i++)
+        fact[i] = mod_mul(fact[i - 1], i);
+}
+
+bool isKthBitSet(ll n, ll k){
+    if ((n >> k) & 1)
+        return true;
+    return false;
+}
+
+bool isPalindrome(string S){
+    string P = S;
+    reverse(all(P));
+    if (S == P) return true;
+    else return false;
+}
+
+void solve(){}
+
+ll fac[200005]; 
+ 
+ll power(ll x, ll y, ll p) 
+{ 
+    ll res = 1;  
+    x = x % p;
+    while (y > 0) 
+    { 
+        if (y & 1) 
+            res = (res*x) % p;  
+        y = y>>1;
+        x = (x*x) % p;   
+    } 
+    return res; 
+}
+ 
+ll nCr(ll n,ll r, ll p = MOD)  
+{ 
+    if(r > n)   {
+        return 0;
+    }
+    if(n < 0 || r < 0)  {
+        return 0;
+    }
+    if (r==0) 
+      return 1;
+    
+  
+    return (fac[n]* power(fac[r], p-2, p) % p * 
+            power(fac[n-r], p-2, p) % p) % p; 
+} 
+ 
+int main()  {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);cout.tie(0);
+    ll t;
+    cin >> t;
+    
+    fac[0] = 1; 
+    for (int i=1 ; i< 200005; i++) 
+        fac[i] = fac[i-1]*i%MOD; 
+        
+    while(t--)	{
+    	ll n, m, k;
+    	cin >> n >> m >> k;
+    	ll a[n];
+    	for(ll i = 0; i < n; i++)	{
+    		cin >> a[i];
+    	}
+    	sort(a, a+n);
+    	ll l = 0;
+    	ll ans = 0;
+    	for(ll r = 0; r < n; r++)	{
+    		while(a[r]-a[l] > k)	{
+    			l++;
+    		}
+    		ans += nCr(r-l, m-1);
+    		ans %= MOD;
+    	}
+    	cout << ans << "\n";
+    }
+    return 0;
+}
