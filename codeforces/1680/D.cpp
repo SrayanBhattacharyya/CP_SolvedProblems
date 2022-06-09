a6b1c8b11fad2318498e38bc9a6fdaee92578913
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-06-09 21:09:00 +0530

    Add solution for problem `D - Dog Walking` Link: https://codeforces.com/contest/1680/problem/D Tags: brute force, greedy, math, *2400 Ref: https://codeforces.com/contest/1680/submission/160032107

---
+++ codeforces/1680/D.cpp
@@ -0,0 +1,186 @@
+//Srayan Bhattachrayya JU EE
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long double
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rev(i,n) for(ll i=n;i>=0;i--)
+#define rev_a(i,a,n) for(ll i=n;i>=a;i--)
+#define rep_a(i,a,n) for(ll i=a;i<n;i++)
+typedef pair <ll, ll> pll;
+typedef vector <ll> vll;
+typedef vector <vll> vvll;
+typedef vector <string> vs;
+typedef set <ll> sll;
+typedef vector <pll> vpll;
+typedef vector <long double> vld;
+#define st(s) sort(s.begin(),s.end())
+#define dst(s) sort(s.begin() , s.end() , greater<int>())
+#define all(s) s.begin(), s.end()
+#define yes cout << "YES" << "\n"
+#define no cout << "NO" << "\n"
+#define print(arr) for (auto it = arr.begin(); it != arr.end(); ++it) cout << *it << " "; cout << "\n";
+const ll MOD = 1e9 + 7; 
+const ll MODS = 998244353; 
+const ll NN = 1e7 + 10; 
+const ll NS = 1e5 + 10;
+const ll LLMAX = 1e18; 
+const lld pi = 3.14159265358979323846;
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+ll lcm(ll a, ll b) {return (a * b) / gcd(a, b);}
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+ll mod_mul(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a * b) % MOD) + MOD) % MOD;}
+ll mods_mul(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a * b) % MODS) + MODS) % MODS;}
+ll mod_add(ll a, ll b) {a = a % MOD; b = b % MOD; return (((a + b) % MOD) + MOD) % MOD;}
+ll mod_inv(ll i) {if (i == 1) return 1; return (MOD - ((MOD / i) * mod_inv(MOD % i)) % MOD) % MOD;}
+ll mods_inv(ll i) {if (i == 1) return 1; return (MODS - ((MODS / i) * mod_inv(MODS % i)) % MODS) % MODS;}
+ll mod_pwr(ll a, ll b) { if(b < 0) return 0; ll res = 1; while (b > 0) {if (b & 1) res = res * a % MOD; a = a * a % MOD; b >>= 1;} return res;}
+ll mod_div(ll a, ll b) {a = a % MOD; ll inv = mod_inv(b); if (inv == -1) return -1; else {ll ans = (inv * a) % MOD; return ans;}}
+ll mods_div(ll a, ll b) {a = a % MODS; ll inv = mod_inv(b); if (inv == -1) return -1; else {ll ans = (inv * a) % MODS; return ans;}}
+
+vs split(string s, string del = " "){
+    vs ans; ll start = 0; ll end = s.find(del);
+    while (end != -1) {
+        cout << s.substr(start, end - start) << "\n";
+        start = end + del.size(); 
+        end = s.find(del, start);
+    }
+    ans.push_back(s.substr(start, end - start)); 
+    return ans;
+}
+
+vll divisors(ll n){
+    vll v;
+    for (ll i = 1; i <= (ll)sqrt(n); i++){
+        if (n % i == 0){
+            v.emplace_back(i);
+            if ((n / i) != i) v.emplace_back(n / i);
+        }
+    }
+    return v;
+}
+
+vll findPrimeFactors(ll n){
+    vll primeFactors;
+    while (n%2== 0) primeFactors.push_back(2), n = n/2;
+    for (ll i = 3; i*i <= n; i = i+2)
+    {
+        while (n%i == 0)
+        {   
+            primeFactors.push_back(i);
+            n = n/i;
+        }
+    }
+    if (n > 2) primeFactors.push_back(n);
+    return primeFactors;
+}
+
+bool isPrime(ll n){
+	if (n == 1) return false;
+    if(n==2 or n==3) return true;
+    if(n%2==0) return false;
+    for(ll i=3; i * i <= n; i += 2) if(n % i==0) return false;
+    return true;
+}
+
+ll *fact;
+void calFact(){
+    fact = new ll[NN];
+    fact[0] = 1;
+    fact[1] = 1;
+    for (ll i = 2; i < NN; i++)
+        fact[i] = mod_mul(fact[i - 1], i);
+}
+ 
+ll ncr(ll n, ll r){
+    ll ans = fact[n];
+    ans = mod_mul(ans, mod_inv(fact[n - r]));
+    ans = mod_mul(ans, mod_inv(fact[r]));
+    return ans;
+}
+
+bool isKthBitSet(ll n, ll k){
+    if ((n >> k) & 1)
+        return true;
+    return false;
+}
+
+bool isPalindrome(string S){
+    string P = S;
+    reverse(all(P));
+    if (S == P) return true;
+    else return false;
+}
+
+void solve(){}
+
+const ll maxn = 500010;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    //cin >> t;
+    while (t--){
+        ll n, k;
+	cin >> n >> k;
+	ll a[maxn], s[maxn];
+	ll z[maxn];
+	rep_a(i, 1, n + 1){
+		cin >> a[i];
+		s[i] = a[i] + s[i-1];
+		z[i] = z[i-1] + (a[i] == 0);
+	}
+	if ((abs(s[n]) + (k-1)) / k > z[n]){
+		cout << -1 << "\n";
+		return 0;
+	}
+	ll b[maxn];
+	ll S = -s[n];
+	int c = (abs(s[n]) + (k - 1)) / k;
+	rep_a(i, 1, c + 1){
+		if (S > 0) b[i] = min(S, k);
+        else {
+			assert(S < 0);
+			b[i] = max(S, -k);
+		}
+		S -= b[i];
+	}
+	for (int i = c + 1; i <= z[n]; i += 2){
+		if (i == z[n]) {
+			b[i] = 0;
+			if (c > 0 && abs(b[c]) != k){
+				if (b[c] < 0){
+					b[i] = k + b[c];
+					b[c] = -k;
+				} 
+                else{
+					b[i] = b[c] - k;
+					b[c] = +k;
+				}
+			}
+		} else {
+			b[i] = +k;
+			b[i+1] = -k;
+		}
+	}
+	int zz = z[n];
+	sort(b + 1, b + 1 + zz);
+	ll B[maxn];
+	for (int i = 1, j = zz; i <= n; ++i, --j) B[i] = b[j];
+	for (int i = 1; i <= zz; ++i){
+		b[i] += b[i-1];
+		B[i] += B[i-1];
+	}
+	ll ans = 0;
+	for (int i = 0; i <= n; ++i) {
+		for (int j = 0; j <= i; ++j) {
+			ans = max(ans, abs(s[i] - s[j] + b[z[i] - z[j]]));
+			ans = max(ans, abs(s[i] - s[j] + B[z[i] - z[j]]));
+		}
+	}
+	cout << ans + 1 << "\n";
+    }    
+    return 0;
+}
