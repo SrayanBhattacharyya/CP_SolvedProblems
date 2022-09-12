8fc8773978c84cdb4005bfe34f5b3768e3c3eda0
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-09-12 22:14:00 +0530

    Add solution for problem `A - Is it rated?` Link: https://codeforces.com/contest/1331/problem/A Tags: *special problem Ref: https://codeforces.com/contest/1331/submission/171939727

---
+++ codeforces/1331/A.cpp
@@ -0,0 +1,13 @@
+//Srayan Bhattachrayya JU EE
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+	cout << "NO";
+    return 0;
+}
