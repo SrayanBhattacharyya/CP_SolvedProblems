420912a731661b453672c2d423d0067480fabfad
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-09-30 23:17:00 +0530

    Add solution for problem `A - Immobile Knight` Link: https://codeforces.com/contest/1739/problem/A Tags: implementation Ref: https://codeforces.com/contest/1739/submission/174159279

---
+++ codeforces/1739/A.cpp
@@ -0,0 +1,18 @@
+//Srayan Bhattachrayya JU EE
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    while (t--){
+        ll n, m;
+        cin >> n >> m;
+        cout << (n + 1) / 2 << ' ' << (m + 1) / 2 << "\n";
+    }    
+    return 0;
+}
