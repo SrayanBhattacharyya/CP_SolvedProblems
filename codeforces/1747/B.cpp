6c7ec7283ca7df2aacd3e09e9e24d3cc378fc683
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-11-04 23:16:00 +0530

    Add solution for problem `B - BAN BAN` Link: https://codeforces.com/contest/1747/problem/B Tags: constructive algorithms, *900 Ref: https://codeforces.com/contest/1747/submission/179295687

---
+++ codeforces/1747/B.cpp
@@ -0,0 +1,18 @@
+//Srayan Bhattachrayya JU EE
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    cin >> t;
+    while (t--){
+        ll n; cin >> n;
+        cout << (n + 1) / 2 << "\n";
+        for(ll i = 1, j = n * 3; i < j; i += 3, j -= 3) cout << i << ' ' << j << "\n";
+    }    
+    return 0;
+}
