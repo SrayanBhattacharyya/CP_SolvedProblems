aaaf864206f68d0aa831eeb7660a161963162efc
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-11-11 20:34:00 +0530

    Add solution for problem `B - Segments` Link: https://codeforces.com/contest/909/problem/B Tags: constructive algorithms, math, *1300 Ref: https://codeforces.com/contest/909/submission/180481984

---
+++ codeforces/909/B.cpp
@@ -0,0 +1,37 @@
+//Srayan Bhattachrayya JU EE
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+const int mod = 1e9 + 7;
+
+ll mod_pwr(ll a, ll b) { 
+	if(b < 0) return 0; 
+	ll res = 1; 
+	while (b > 0) {
+		if (b & 1) res = res * a % mod; 
+		a = a * a % mod; 
+		b >>= 1;
+	} 
+	return res;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll t = 1;
+    //cin >> t;
+    while (t--){
+		ll n;
+		cin >> n;
+		ll o = (n - 1) / 2 + 1;
+		ll ans = 0;
+		for (ll i = 0; i < o; i++) {
+			ans += n;
+			n -= 2;
+		}
+		cout << ans << '\n';
+    }    
+    return 0;
+}
