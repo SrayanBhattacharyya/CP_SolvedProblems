4606835ff8e0cc7424b4e848199eab5328958c2c
Srayan Bhattcharyya <srayanbhatta123@gmail.com>
2022-11-15 20:37:00 +0530

    Add solution for problem `G - Petya's Exams` Link: https://codeforces.com/contest/978/problem/G Tags: greedy, implementation, sortings, *1700 Ref: https://codeforces.com/contest/978/submission/181107093

--- codeforces/978/G.cpp
+++ codeforces/978/G.cpp
@@ -3,43 +3,41 @@
 using namespace std;
 #define ll long long
 
+vector <pair <pair <ll, ll>, pair <ll, ll>>> a;
+vector <ll> ans;
+
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
     ll n, m; cin >> n >> m;
-	vector <pair <ll, pair <ll, ll>>> u;
-	map <ll, ll> mp;
+	a.resize(m); ans.resize(n + 1);
+	ll flag = 1;
 	for (ll i = 0; i < m; i++) {
-		ll x, y, z;
-		cin >> x >> y >> z;
-		u.push_back({y, {x, z}});
-		mp[y] = i + 1;
-	}
-	ll ans[n + 1];
-	for (ll i = 0; i <= n; i++) {
-		if (mp.count(i)) ans[i] = m + 1;
-		else ans[i] = 0;
-	}
-	ranges::sort(u);
-	for (ll i = 0; i < u.size(); i++) {
-		ll exam = u[i].first;
-        ll start = u[i].second.first;
-        ll days = u[i].second.second;
-        ll cnt = 0;
-		for (ll j = start; j < exam; j++) {
-			if (cnt == days) break;
-			if (ans[j] == 0) {
-				ans[j] = mp[exam];
-				cnt++;
-			}
-		}
-		if (cnt != days) {
-			cout << -1 << "\n";
-			return 0;
-		}
-	}
-	for (ll i = 1; i <= n; i++) cout << ans[i] << " ";
-	cout << "\n";
+		cin >> a[i].first.second >> a[i].first.first >> a[i].second.first;
+    	a[i].second.second = i+1;
+    	ans[a[i].first.first] = m + 1;
+    	if (a[i].second.first > a[i].first.first - a[i].first.second) flag = 0;
+    }
+	ranges::sort(a);
+	for(ll i = 0; i < m; i++){
+    	ll st = a[i].first.second;
+    	ll dl = a[i].first.first;
+    	ll req = a[i].second.first;
+    	ll v = a[i].second.second;
+    	for (ll j = st; j < dl; j++) {
+    		if(ans[j] == 0){
+    			ans[j]=v;
+    			req--;
+    			if(req == 0) break;
+    		}
+    	}
+    	if (req > 0) {
+    		flag = 0;
+    		break;
+    	}
+    }
+	if (flag) for (ll i = 1; i <= n; i++) cout << ans[i] << " ";
+    else cout << -1 << "\n";
     return 0;
 }
